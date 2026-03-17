474bfb65b8858bea654ec43edd0a44316dfeb52d
Sheng-Bi Jiang <shengbi.jiang@gmail.com>
2026-03-17 22:50:37 +0800

    feat: validate parameter saltRounds

--- src/users/bcrypt-password-hasher.ts
+++ src/users/bcrypt-password-hasher.ts
@@ -5,6 +5,9 @@ export class BcryptPasswordHasher implements PasswordHasher {
     private readonly saltRounds: number;
 
     public constructor(saltRounds = 10) {
+        if (!Number.isInteger(saltRounds) || saltRounds < 10) {
+            throw new Error('saltRounds must be an integer >= 10');
+        }
         this.saltRounds = saltRounds;
     }
 
